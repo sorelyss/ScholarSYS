5ee672db68df62065222a0ba070703bc571e50ee
Sorelys <sorboy11@gmail.com>
2017-02-02 15:34:24 -0500

    Login and Registration of users complete

--- apps/Users/urls.py
+++ apps/Users/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import login
 
 urlpatterns = [
     url(r'^$', views.Home, name = 'home'),
     url(r'^register_user', views.Register_user.as_view(), name = 'register_user'),
-    url(r'^login_user', views.login, name = 'login_user'),
+    url(r'^login_user', login, {'template_name':'users/login.html'}, name = 'login_user'),
 ]
 
 # {'template_name': 'users/login.html','redirect_field_name': '/'}
--- apps/Users/views.py
+++ apps/Users/views.py
@@ -4,14 +4,10 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.views.generic import CreateView
 from django.core.urlresolvers import reverse_lazy
 from apps.Users.forms import UserRegisterForm
-from django.contrib.auth.views import login
 
 
 def Home(request):
 	return render(request, 'home.html')
-
-def login(request,redirect_authenticated_user=False):
-	return render(request, 'users/login.html', {'form':AuthenticationForm,'next':'/register_user'})
 	
 class Register_user(CreateView):
 	model = User
